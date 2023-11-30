fd43a7bd195f7f582fa36c532b39e57edd1ef6d1
Maulana Imanulhaq <imanulhaq5@gmail.com>
2023-11-30 16:59:31 +0700

    first commit

--- test.js
+++ test.js
@@ -1,3 +1,3 @@
-console.log("dari index js");
+console.log("AKU MAULANA dari index js");
 
-document.write("coba nulis disini test dari index js");
+document.write("AKU MAULANA coba nulis disini test dari index js");
